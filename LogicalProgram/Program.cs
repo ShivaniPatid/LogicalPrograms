ddfa987a99d716cd63402aa487049031a2bdc99d
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-29 19:39:17 +0530

    [Shivani] 4. Print Reverse Number

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -7,7 +7,7 @@ namespace LogicalProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome user...");
-            Console.WriteLine("Choose Number\n 1. Fibonacci Series\t 2. Perfect Number\t 3. Prime Number");
+            Console.WriteLine("Choose Number\n 1. Fibonacci Series\t 2. Perfect Number\t 3. Prime Number\t 4. Reverse Number");
             int num=Convert.ToInt32(Console.ReadLine());
 
             switch (num)
@@ -20,6 +20,9 @@ namespace LogicalProgram
                     break;
                 case 3:
                     PrimeNumber.CheckPrimeNumber();
+                    break;
+                case 4:
+                    ReverseNumber.PrintReverseNumber();
                     break;
                 default:
                     Console.WriteLine("Choose Rigth Number");
---
+++ LogicalProgram/ReverseNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LogicalProgram
+{
+    internal class ReverseNumber
+    {
+        public static void PrintReverseNumber()
+        {
+            int num, reminder, reverse = 0;
+            Console.WriteLine("Enter the Number");
+            num=Convert.ToInt32(Console.ReadLine());
+
+            while (num > 0)
+            {
+                reminder = num % 10;
+                reverse = reverse * 10 + reminder;
+                num /= 10;
+            }
+            Console.WriteLine("Reverse number : "+reverse);
+        }
+        
+
+    }
+}
