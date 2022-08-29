ccd1aecbaa825bd425397e76091a8f1e8daabffc
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-29 18:38:12 +0530

    [Shivani] Welcome User

---
+++ LogicalProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LogicalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome user...");
+            Console.WriteLine("Choose Number\n 1. Fibonacci Series");
+            int num=Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
