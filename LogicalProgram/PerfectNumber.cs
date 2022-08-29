6387a4144b6649ba122f91890679811d31b3f448
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-29 19:08:17 +0530

    [Shivani] 2. Check Perfect Number

---
+++ LogicalProgram/PerfectNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace LogicalProgram
+{
+    internal class PerfectNumber
+    {
+        public static void CheckPerfectNumber()
+        {
+            int num, rem, sum = 0, i;
+            Console.WriteLine("Enter the Number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i < num; i++)
+            {
+                rem = num % i;
+                if (rem == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == num)
+            {
+                Console.WriteLine(num+" is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine(num+" is not a prefect number");
+            }
+        }
+    }
+}
--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -7,13 +7,16 @@ namespace LogicalProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome user...");
-            Console.WriteLine("Choose Number\n 1. Fibonacci Series");
+            Console.WriteLine("Choose Number\n 1. Fibonacci Series\t 2. Perfect Number");
             int num=Convert.ToInt32(Console.ReadLine());
 
             switch (num)
             {
                 case 1:
                     FibonacciSeries.PrintFibonacciSeries();
+                    break;
+                case 2:
+                    PerfectNumber.CheckPerfectNumber();
                     break;
                 default:
                     Console.WriteLine("Choose Rigth Number");
