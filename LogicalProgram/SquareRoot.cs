d2b62f51897f5246c28da37e2ee1ed08f6eca2bb
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-31 21:06:58 +0530

    [Shivani] 8. Square Root

---
+++ LogicalProgram/SquareRoot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class SquareRoot
+    {
+        public static void CalculateSquareRoot()
+        {
+            Console.WriteLine("Enter the number");
+            int c=Convert.ToInt32(Console.ReadLine());
+
+
+            int t = c;
+            float epsilon = 0.00001f;  // where epsilon = 1e - 15
+            if (Math.Abs(t - c / t) > epsilon * t)
+            {
+                Console.WriteLine(Math.Sqrt(c));
+               
+            }
+        }
+    }
+}
