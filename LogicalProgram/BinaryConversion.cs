b827b9fc59b28bd06fde09e288c3a5273868a7eb
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-31 21:10:05 +0530

    [Shivani] 9. Binary Conversion

---
+++ LogicalProgram/BinaryConversion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class BinaryConversion
+    {
+        public static void ToBinary()
+        {
+            string result;
+
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            result = "";
+            while (num > 1)
+            {
+                int reminder = num % 2;
+                result = Convert.ToString(reminder) + result;
+                num /= 2;
+            }
+            result=Convert.ToString(num)+result;
+            Console.WriteLine("Binarey : "+result);
+
+        }
+    }
+}
