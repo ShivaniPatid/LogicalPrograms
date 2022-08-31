a82c26c6cfdbdb61e983e8f94090c4c33d130a39
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-31 21:12:59 +0530

    [shivani] 10. Swap nibbles

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -7,7 +7,7 @@ namespace LogicalProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome user...");
-            Console.WriteLine("Choose Number\n 1. Fibonacci Series\t 2. Perfect Number\t 3. Prime Number\t 4. Reverse Number");
+            Console.WriteLine("Choose Number\n 1. Fibonacci Series\t 2. Perfect Number\t 3. Prime Number\t 4. Reverse Number\t 5. Day of Week\n 6. Temperatur Conversion\t 7. Monthly payment\t 8. Square Root\t 9. Binary Conversion\t 10.Swap nibbles");
             int num=Convert.ToInt32(Console.ReadLine());
 
             switch (num)
@@ -24,6 +24,25 @@ namespace LogicalProgram
                 case 4:
                     ReverseNumber.PrintReverseNumber();
                     break;
+                case 5:
+                    DayOfWeek.DayOrMonth();
+                    break;
+                case 6:
+                    TemperaturConversion.Temperature();
+                    break;
+                case 7:
+                    MonthlyPayment.CalculateMonthlyPayment();
+                    break;
+                case 8:
+                    SquareRoot.CalculateSquareRoot();
+                    break;
+                case 9:
+                    BinaryConversion.ToBinary();
+                    break;
+                case 10:
+                    Console.WriteLine("New number is "+SwapNibbles.SwappingNibbles(100));
+                    break;
+
                 default:
                     Console.WriteLine("Choose Rigth Number");
                     break;
---
+++ LogicalProgram/SwapNibbles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class SwapNibbles
+    {
+        public static int SwappingNibbles(int x)
+        {
+
+            return ((x & 0x0F) << 4 | (x & 0xF0) >> 4);
+           
+        }
+    }
+}
