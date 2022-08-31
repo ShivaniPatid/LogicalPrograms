99fb785770e9a9b254a2eaf794b6111faa44cee0
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-31 21:01:56 +0530

    [Shivani] 6. Temperatur Conversion

---
+++ LogicalProgram/TemperaturConversion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class TemperaturConversion
+    {
+        public static void Temperature()
+        {
+            Console.WriteLine("Enter Celsius temperature");
+            double c=Convert.ToDouble(Console.ReadLine());
+
+            double f = (c * 9 / 5) + 32;
+            Console.WriteLine("Calsius to Fahrenheit : "+f);
+
+            c = (f - 32) * 5 / 9;
+            Console.WriteLine("Fahrenheit to Celsius : "+c);
+        }
+    }
+}
